8acee8194552c1fc82b8198d72015e0dee33ac58
Jasper Francisco <jasper.francisco@gmail.com>
2020-04-23 10:29:49 +0800

    Add tests for user input validation

--- test/Gradebook.Tests/BooksTests.cs
+++ test/Gradebook.Tests/BooksTests.cs
@@ -21,6 +21,19 @@ namespace GradeBook.Tests
             Assert.Equal(85.6, result.Average, 1);
             Assert.Equal(90.5, result.High);
             Assert.Equal(77.3, result.Low);
+            Assert.Equal('B', result.Letter);
+        }
+
+        [Fact]
+        public void OnlyValidGradesCanBeAdded()
+        {
+            var book = new Book("");
+            book.AddGrade(85.00);
+            Action beyondRangeGrade = () => book.AddGrade(101.00);
+            Action belowRangeGrade = () => book.AddGrade(-1.00);
+
+            Assert.Throws<ArgumentException>(beyondRangeGrade);
+            Assert.Throws<ArgumentException>(belowRangeGrade);
         }
     }
 }
